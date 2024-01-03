79785b56137d4c60c0a973172d6463c23dbac83c
Diego Gauna <diego_g_pc@hotmail.com>
2024-01-03 20:08:42 -0300

    ejercicios de clase

---
+++ Comision50285/Desafio2DiegoGauna/Program.cs
@@ -0,0 +1,48 @@
+﻿
+int pass = 12345678;
+string perfil = "A";
+
+Console.WriteLine("Bienvenido");
+Console.WriteLine("Ingrese su contrseña");
+
+if (Convert.ToInt32(Console.ReadLine()) == pass)
+{
+    switch (perfil)
+    {
+        case "A":
+            Console.WriteLine("Bienvenido Administrador");
+            break;
+        case "B":
+            Console.WriteLine("Bienvenido Usuario");
+            break;
+        default:
+            Console.WriteLine("Sin perfil definido");
+            break;
+    }
+}
+else
+{
+    Console.WriteLine("Acceso denegado. Ingrese nuvamente la clave");
+    
+    if (Convert.ToInt32(Console.ReadLine()) == pass)
+    {
+        switch (perfil)
+        {
+            case "A":
+                Console.WriteLine("Bienvenido Administrador");
+                break;
+            case "B":
+                Console.WriteLine("Bienvenido Usuario");
+                break;
+            default:
+                Console.WriteLine("Sin perfil definido");
+                break;
+        }
+
+    }
+    else
+    {
+        Console.WriteLine("Debe reiniciar el sistema");
+    }
+}
+Console.ReadKey();
---
+++ Comision50285/clase2Desafio1DiegoGauna/Program.cs
@@ -0,0 +1,50 @@
+﻿
+Console.WriteLine("BIENVENIDOS A LA UNIVERSIDAD CODERHOUSE!!!!!");
+Console.WriteLine(" ");
+Console.WriteLine("EL MEJOR LUGAR PARA ESTUDIAR DEL MUNDO");
+Console.WriteLine(" ");
+Console.WriteLine("INGRESE LA COMISION");
+
+String comision = Console.ReadLine();
+int nota = 0;
+
+switch (comision)
+{
+	case "A":
+        Console.WriteLine("Turno Mañana");
+        break;
+    case "B":
+        Console.WriteLine("Turno Tarde");
+        break;
+    case "C":
+        Console.WriteLine("Turno Noche");
+        break;
+
+    default:
+        Console.WriteLine("Sin turno fijo");
+		break;
+}
+
+
+Console.WriteLine("BIENVENIDOS A LA UNIVERSIDAD CODERHOUSE!!!!!");
+Console.WriteLine(" ");
+Console.WriteLine("EL MEJOR LUGAR PARA ESTUDIAR DEL MUNDO");
+Console.WriteLine(" ");
+Console.WriteLine("INGRESE LA NOTA DEL ALUMNO");
+
+nota = Convert.ToInt32(Console.ReadLine());
+
+if ( nota < 4)
+{
+    Console.WriteLine("Recursa la materia");
+
+}else if(nota >= 4 && nota <= 7)
+{
+    Console.WriteLine("Aprobó cursada");
+}
+else
+{
+    Console.WriteLine("Promociona la materia");
+}
+
+Console.ReadKey();
